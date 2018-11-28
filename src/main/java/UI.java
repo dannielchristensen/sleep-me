a7f41213c577d71e1acf4d4b8c28cdcd4c115ac0
Danniel <44185315+dannielchristensen@users.noreply.github.com>
2018-11-27 20:31:03 -0600

    Update UI.java

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,2 +1,19 @@
+// I didnt know what kind of interface we wanted, so this is all console, if we decide to switch to GUI it wont be hard
+import java.util.*;
 public class UI {
+  private Scanner s;
+  public UI(){
+    s = new Scanner();
+  }
+  //reads in the dream info (will only take one paragraph at a time)
+  public String takeDream(){
+     String para = s.nextLine();
+     return para;
+  }
+  //displays one paragraph
+  public displayDream(String a){
+     System.out.println("DREAM: ");
+     System.out.println(a); 
+  }
+  
 }
